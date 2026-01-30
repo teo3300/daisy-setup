dc8b25bed197b45a0ded8c917c683458aaa9e056
teo3300 <teo3300>
2026-01-30 12:12:38 +0100

    Yei

--- habenaria/makecerts.sh
+++ habenaria/makecerts.sh
@@ -13,9 +13,9 @@ if [ -z "$SERVER_SUBDOMAINS" ]; then
   echo You can specify the subdomains in \$SERVER_SUBDOMAINS
 fi
 
-# Run an instance of nginx with limited config (no 443), so it can run with certbot without failing for missing certificates
-docker compose down nginx
-docker compose up -d nginx-certbot
+# Stop nginx if running
+docker container stop nginx
+docker container rm nginx
 
 echo Creating cert for: $SERVER_DOMAIN
 SERVER_SUBDOMAIN="$SERVER_DOMAIN" docker compose up certbot-new
@@ -24,5 +24,5 @@ for SUBDOMAIN in ${(s: :)SERVER_SUBDOMAINS}; do
   echo Creating cert for: $SUBDOMAIN.$SERVER_DOMAIN
   SERVER_SUBDOMAIN="$SUBDOMAIN.$SERVER_DOMAIN" docker compose up certbot-new
 done
-docker compose down nginx-certbot
+
 docker compose up -d nginx

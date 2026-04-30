0215e1d2a1b118b1e470e2b2575a6066cf59e30f
teo3300 <teo3300>
2026-04-30 17:59:11 +0200

    Added password protection nd geoblock to nginx

--- habenaria/nginx/makeconf.sh
+++ habenaria/nginx/makeconf.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+# COUNTRY_WHITELIST="US NL ..."
+# Whitelist countries
+COUNTRIES=""
+for c in $COUNTRY_WHITELIST; do
+    COUNTRIES+="$c 0; "
+done
+
+export COUNTRY_WHITELIST=$COUNTRIES
+
+# Ugly ass templated macros
+export GEOBLOCK='if ($geo_block) { return 444; }'
+
+export PASSWORD='auth_basic "Protected"; auth_basic_user_file /etc/nginx/.htpasswd;'
+
 # You must list here what variables to substitute (to avoid breaking regex)
 
 envsubst\
-  '${SERVER_DOMAIN}'\
+  '${SERVER_DOMAIN} ${COUNTRY_WHITELIST} ${GEOBLOCK} ${WG_NET} ${PASSWORD}'\
   < /etc/nginx/template-nginx.conf > /etc/nginx/nginx.conf

59502dd3cee7cd0a42d4af24f7a7c9c51a78d8db
teo3300 <teo3300>
2026-04-30 17:57:16 +0200

    Fixing cloudflare update script

--- habenaria/dnsup/cloudflare.sh
+++ habenaria/dnsup/cloudflare.sh
@@ -8,7 +8,7 @@ source $HOME/.env
 IP=$(curl -s ipinfo.io/ip)
 
 # Make sure CLOUDFLARE_DN_IDS are in the same order as
-domains=("@" "*")
+domains=(@ *)
 
 for ((i=0; i<${#CLOUDFLARE_DNS_IDS[@]}; i++)); do
     id=${CLOUDFLARE_DNS_IDS[$i]}
@@ -16,13 +16,13 @@ for ((i=0; i<${#CLOUDFLARE_DNS_IDS[@]}; i++)); do
     curl -s https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records/$id \
         -X PUT \
         -H 'Content-Type: application/json' \
-        -H "Authorization: Bearer "$CLOUDFLARE_API_TOKEN \
+        -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
         -d '{
               "ttl": 3600,
               "name": "'${name}'",
               "type": "A",
               "content": "'${IP}'",
-              "proxied": true
-           }'\
+              "proxied": false 
+           }' \
        | jq 'select (.errors != [])'
 done
